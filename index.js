f732851fba31233ef4b897ad55b8d458bf321373
Thiago dos Santos <thidosantos@gmail.com>
2023-02-09 12:47:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 
-const DB_URL = "mongodb://localhost:27017";
-const DB_NAME = "nodejs-ocean-fullstack-23";
+// localhost ou 127.0.0.1
+const DB_URL = "mongodb://127.0.0.1:27017";
+const DB_NAME = "ocean-bancodados-09-02-2023";
 
-async function main(){
-// Conexão com o banco de dados
-console.log("Conectando com o banco de dados...");
-const client = MongoClient.connect(DB_URL);
-console.log("Banco de dados conectado com sucesso!");
+async function main() {
+  // Conexão com o banco de dados
+  console.log("Conectando com o banco de dados...");
+  const client = await MongoClient.connect(DB_URL);
+  const db = client.db(DB_NAME);
+  const collection = db.collection("itens");
+  console.log("Banco de dados conectado com sucesso!");
 
-const app = express();
+  const app = express();
 
-// O que vier no body da requisição, está em JSON
-app.use(express.json());
+  // O que vier no body da requisição, está em JSON
+  app.use(express.json());
 
-// Endpoint / -> Hello World
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+  // Endpoint / -> Hello World
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
 
-// Endpoint /oi -> Olá, mundo!
-app.get("/oi", function (req, res) {
-  res.send("Olá, mundo!");
-});
+  // Endpoint /oi -> Olá, mundo!
+  app.get("/oi", function (req, res) {
+    res.send("Olá, mundo!");
+  });
 
-// Lista de informações
-const itens = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
-//              0               1              2
+  // Lista de informações
+  const itens = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
+  //              0               1              2
 
-// CRUD -> Lista de informações
+  // CRUD -> Lista de informações
 
-// Endpoint Read All -> [GET] /item
-app.get("/item", function (req, res) {
-  res.send(itens);
-});
+  // Endpoint Read All -> [GET] /item
+  app.get("/item", async function (req, res) {
+    const documentos = await collection.find().toArray();
+    res.send(documentos);
+  });
 
-// Endpoint Read Single by ID -> [GET] /item/:id
-app.get("/item/:id", function (req, res) {
-  const id = req.params.id;
-  const item = itens[id - 1];
-  res.send(item);
-});
+  // Endpoint Read Single by ID -> [GET] /item/:id
+  app.get("/item/:id", function (req, res) {
+    const id = req.params.id;
+    const item = itens[id - 1];
+    res.send(item);
+  });
 
-// Endpoint Create -> [POST] /item
-app.post("/item", function (req, res) {
-  // console.log(req.body);
-  const item = req.body;
-  itens.push(item.nome);
-  res.send("Item criado com sucesso!");
-});
+  // Endpoint Create -> [POST] /item
+  app.post("/item", function (req, res) {
+    // console.log(req.body);
+    const item = req.body;
+    itens.push(item.nome);
+    res.send("Item criado com sucesso!");
+  });
 
-app.listen(3000);
+  app.listen(3000);
 }
 
 main();
